51440fe85357685a8f2d93ec4922efb16b84c793
mixa2130 <mixa21.11@mail.ru>
2023-06-03 19:48:00 +0300

    Events schema config update

--- src/events/schemas.py
+++ src/events/schemas.py
@@ -15,10 +15,21 @@ class EventSchema(BaseModel):
 
 class AddEventSchema(BaseModel):
     name: str
-    date: datetime
+    date: datetime = Field(description="ISO 8601 formatted date time")
     place: str
     price: float = Field(ge=0.0, default=0.0)
     max_players: int = Field(ge=1, default=100)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Runderground jubilee event 2023",
+                "date": "2023-02-20T18:02:13Z",
+                "place": "Moscow, mukomolny passage 2",
+                "price": 2400,
+                "max_players": 77
+            }
+        }
 
 
 class EventAlreadyExistsSchema(BaseModel):
